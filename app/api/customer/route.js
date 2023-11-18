670de4cbbd8b3ff04f416fdbd75a2eac873c0624
peteCoder <petercodercoder@gmail.com>
2023-11-18 01:59:57 -0800

    fixed error regarding message sending

--- app/api/customer/route.js
+++ app/api/customer/route.js
@@ -16,7 +16,6 @@ export const POST = async (res) => {
     phone: phoneNumber,
     email,
     message,
-    // subject,
   };
 
   const response = await sanityClient.create(doc);
@@ -25,45 +24,68 @@ export const POST = async (res) => {
 
   // nodemailer transporter instantiation
   const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
     service: process.env.SMTP_EMAIL_PROVIDER,
     auth: {
       user: process.env.SMTP_EMAIL_USER,
       pass: process.env.SMPT_EMAIL_PASSWORD,
     },
+    secure: true,
+  });
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
   });
 
   // Mail Options
   const mailOptions = {
-    from: email,
+    from: {
+      name: `${firstName} ${lastName}`,
+      address: email,
+    },
     to: process.env.SMTP_EMAIL_RECEPIENT,
     subject: `Message from ${firstName} ${lastName}, a customer of Primestix.`,
     text: `Customer email: ${email}. \n\n Customer message: ${message}`,
+    html: `<h2>${message}</h2>`,
   };
 
   if (_id) {
     // Send mail if the
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-        return NextResponse.json(
-          {
-            message: "Message was not submitted successful. Please try again.",
-            status: 400,
-          },
-          { status: 400 }
-        );
-      } else {
-        console.log("Email sent: " + info.response);
-        return NextResponse.json(
-          { message: "Message was sent successful.", status: 200 },
-          { status: 200 }
-        );
-      }
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+          return NextResponse.json(
+            {
+              message:
+                "Message was not submitted successful. Please try again.",
+              status: 400,
+            },
+            { status: 400 }
+          );
+        } else {
+          console.log(info);
+          resolve(info);
+          console.log("Email sent: " + info.response);
+          return NextResponse.json(
+            { message: "Message was sent successful.", status: 200 },
+            { status: 200 }
+          );
+        }
+      });
     });
-    return NextResponse.json(
-      { message: "Message was sent successful.", status: 200 },
-      { status: 200 }
-    );
   } else {
     return NextResponse.json(
       {
@@ -73,4 +95,12 @@ export const POST = async (res) => {
       { status: 400 }
     );
   }
+
+  return NextResponse.json(
+    {
+      message: "Thank you!",
+      status: 200,
+    },
+    { status: 200 }
+  );
 };
