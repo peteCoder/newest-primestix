3c444d66bd0019a10b0cc68b5fd6f3cc93abc8dd
peteCoder <petercodercoder@gmail.com>
2023-11-20 04:18:48 -0800

    set the other news daat

--- app/news/[newsId]/page.js
+++ app/news/[newsId]/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { getAllNewsExceptId } from "@/actions/getAllNewsExceptId";
 import { getDetailNews } from "@/actions/getDetailNews";
 import Image from "next/image";
 import { useParams } from "next/navigation";
@@ -9,6 +10,7 @@ import BeatLoader from "react-spinners/BeatLoader";
 
 const NewsPageDetail = () => {
   const [news, setNews] = useState({});
+  const [otherNews, setOtherNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const params = useParams();
 
@@ -19,7 +21,9 @@ const NewsPageDetail = () => {
       try {
         setIsLoading(true);
         const data = await getDetailNews(newsId);
+        const otherNewsData = await getAllNewsExceptId(newsId);
         setNews(data[0]);
+        setOtherNews(otherNewsData);
         console.log(data[0]);
       } catch (error) {
         console.log(error);
@@ -32,7 +36,7 @@ const NewsPageDetail = () => {
   }, [newsId]);
 
   return (
-    <div className="flex items-center justify-center min-h-[200vh]">
+    <div className="flex items-center justify-center min-h-[500vh] flex-col">
       {isLoading ? (
         <div className="max-w-[970px] lg:max-w-[1170px] mx-auto flex items-center justify-center min-h-[40vh]">
           <BeatLoader color="#e8d5a1b8" />
@@ -60,6 +64,35 @@ const NewsPageDetail = () => {
           </div>
         </div>
       )}
+
+      {/* Here are the other news data */}
+      <div>
+        {/* Other related news data */}
+        <h2 className="text-3xl mt-4">Other news</h2>
+        <div>
+          {otherNews.map((news) => (
+            <div key={news._id} className="border border-t mb-5">
+              <div>
+                {/* Title */}
+                <div>Title: {news?.title}</div>
+                <div>Body: {news?.body}</div>
+                <div className="flex items-center gap-2">
+                  Date: <FormattedDate createdAt={news?._createdAt} />
+                </div>
+                <div>
+                  Image: {news?.bannerImage?.asset?.url}
+                  <Image
+                    width={800}
+                    height={800}
+                    alt="banner-image"
+                    src={news?.bannerImage?.asset?.url}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
